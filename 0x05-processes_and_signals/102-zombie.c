f2f81dd467cea785eba517c5c8f868e5c6493a42
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-10-27 19:48:38 +0100

    add advanced

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * infinite_while - loop forever
+ *
+ * Return: Never
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		printf("hacked\n");
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - spawn zombie processes
+ *
+ * Return: Never
+ */
+int main(void)
+{
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (!pid)
+			return (EXIT_SUCCESS);
+		printf("Zombie process created, PID: %d\n", pid);
+	}
+	return (infinite_while());
+}
--- 0x05-processes_and_signals/README.md
+++ 0x05-processes_and_signals/README.md
@@ -1,1 +1,1 @@
-Readme
+0x05-processes_and_signals
