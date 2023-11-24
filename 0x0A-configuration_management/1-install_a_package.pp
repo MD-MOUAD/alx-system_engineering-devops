f8c921a7971942b9c06d2ddfb5904ae912b74150
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-25 00:57:21 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-# install flask from pip3
-package { 'flask':
+# 1-install_a_package.pp
+
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
+
