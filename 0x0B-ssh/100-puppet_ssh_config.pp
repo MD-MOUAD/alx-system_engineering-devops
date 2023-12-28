302540efbd7ae70c2e47fb85955658a7b9010c62
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-28 19:17:11 +0100

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 # Client configuration file
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  line   => '    PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  ensure => present,
-  line   => '    IdentityFile ~/.ssh/school',
   path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
