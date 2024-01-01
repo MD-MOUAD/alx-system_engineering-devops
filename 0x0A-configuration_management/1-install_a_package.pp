239d39698d95888c5cb6235610578fbae22a3fbf
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-01 19:40:54 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,23 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
+
+class { 'python':
+  version => 'present',
+}
+
+package { 'python3-pip':
+  ensure   => 'present',
+  provider => 'pip3',
+  require  => Class['python'],
+}
+
+package { 'Werkzeug':
+  ensure   => 'latest',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
